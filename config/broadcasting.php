4ec09c959b625d1cd75b0ad0ffa6d95c876cc8b6
MadForCoding <hnishad020@gmail.com>
2026-09-02 16:45:28 +0530

    Add User-Agent for Reverb broadcasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -42,7 +42,9 @@ return [
                 'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'headers' => [
+                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                ],
             ],
         ],
 
